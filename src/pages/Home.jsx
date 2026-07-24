e76a603a9a326a536eb2bc8be1c98fd3f35ff0b5
dcossiodev <dayana-cossio@hotmail.com>
2026-07-24 14:50:12 -0500

    Mejora secciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import About from '../components/sections/About';
 import Projects from '../components/sections/Projects';
 import Skills from '../components/sections/Skills';
 import Experience from '../components/sections/Experience';
+import Education from '../components/sections/Education';
 import Contact from '../components/sections/Contact';
 import Footer from '../components/sections/Footer';
 
